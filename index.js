f8f7647174b5ce954c55216439813404adfa45bf
Hannah Drury <hannah_drury@yahoo.com>
2019-12-07 23:43:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function add()
 {
-  
+  number += 5
 }
 
 function subtract()
 {
-  
+  number -= 5
 }
 
 function multiply()
 {
-  
+  number *= 5
 }
 
 function divide()
 {
-  
+  number /= 5
 }
 
