ffd5b2e8b8584a9790988dd4ad461278e55f6c49
Hannah Drury <hannah_drury@yahoo.com>
2019-12-08 00:09:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,12 +30,12 @@ function dec(n)
   return n
 }
 
-function makeInt('n')
+function makeInt(n)
 {
-  return parseInt('n', 10)
+  return parseInt(n, 10)
 }
 
-function preserveDecimal('n')
+function preserveDecimal(n)
 {
- return  parseFloat('n')
+ return  parseFloat(n)
 }
